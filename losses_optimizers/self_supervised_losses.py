6d57962a7c3281f978cfc53f2178d9cc05712f55
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-10-17 13:08:05 +0800

    commit

--- losses_optimizers/self_supervised_losses.py
+++ losses_optimizers/self_supervised_losses.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.python.ops.gen_batch_ops import batch
 import tensorflow_addons as tfa
 import numpy as np
+from tensorflow.keras import layers
 
 ######################################################################################
 '''Supervised  Contrastive LOSS'''
@@ -12,7 +13,6 @@ def multiclass_npair_loss(z, y):
     '''
     arg: z, hidden feature vectors(B_S[z], n_features)
     y: ground truth of shape (B_S[z])
-
     '''
     # Cosine similarity matrix
     z = tf.math.l2_normalize(z,  axis=1)
@@ -57,12 +57,12 @@ def multiclass_N_pair_loss(p, z):
 '''SimCLR Paper Nt-Xent Loss # ASYMETRIC Loss'''
 # Nt-Xent ---> N_Pair loss with Temperature scale
 # Nt-Xent Loss (Remember in this case dataset two image stacked)
+# This implementation for the Special input data
 
 
 def nt_xent_asymetrize_loss(z,  temperature):
     '''The issue of design this loss two image is in one array
     when we multiply them that will lead two two same things mul together???
-
     '''
     # Feeding data (ALready stack two version Augmented Image)[2*bs, 128]
     z = tf.math.l2_normalize(z, axis=1)
@@ -74,7 +74,7 @@ def nt_xent_asymetrize_loss(z,  temperature):
     ij_indices = tf.reshape(tf.range(z.shape[0]), shape=[-1, 2])
     ji_indices = tf.reverse(ij_indices, axis=[1])
 
-    #[[0, 1], [1, 0], [2, 3], [3, 2], ...]
+    # [[0, 1], [1, 0], [2, 3], [3, 2], ...]
     positive_indices = tf.reshape(tf.concat(
         [ij_indices, ji_indices], axis=1), shape=[-1, 2])  # Indice positive pair
     # --> Output N-D array
@@ -92,9 +92,10 @@ def nt_xent_asymetrize_loss(z,  temperature):
     return tf.reduce_mean(losses)
 
 
-'''SimCLR paper Asytemrize_loss V2'''
+'''SimCLR paper Asytemrize_loss'''
 
 # Mask to remove the positive example from the rest of Negative Example
+
 
 def get_negative_mask(batch_size):
     # return a mask that removes the similarity score of equal/similar images
@@ -108,39 +109,58 @@ def get_negative_mask(batch_size):
     return tf.constant(negative_mask)
 
 
+# Cosine Similarity distance loss measurement
 consie_sim_1d = tf.keras.losses.CosineSimilarity(
     axis=1, reduction=tf.keras.losses.Reduction.NONE)
 cosine_sim_2d = tf.keras.losses.CosineSimilarity(
     axis=2, reduction=tf.keras.losses.Reduction.NONE)
 
+# Official Implementation NCE Loss (Noisy Contrasitve Estimator Loss)
 
-def nt_xent_asymetrize_loss_v2(p, z,  temperature, negative_mask):  # negative_mask
+
+def nt_xent_asymetrize_loss_v2(p, z, temperature, batch_size):  # negative_mask
     # L2 Norm
-
     p_l2 = tf.math.l2_normalize(p, axis=1)
     z_l2 = tf.math.l2_normalize(z, axis=1)
-    similarity = tf.matmul(tf.expand_dims(p_l2, 1), tf.expand_dims(z_l2, 2))
-    batch_size = tf.shape(p, out_type=tf.dtypes.int32)[0]
-    print(batch_size)
-    similarity = (tf.reshape(similarity, (batch_size, 1)))/temperature
+
+    # Cosine Similarity distance loss
+
+    # pos_loss = consie_sim_1d(p_l2, z_l2)
+    pos_loss = tf.matmul(tf.expand_dims(p_l2, 1), tf.expand_dims(z_l2, 2))
+
+    pos_loss = tf.reshape(pos_loss, (batch_size, 1))
+    pos_loss /= temperature
     negatives = tf.concat([p_l2, z_l2], axis=0)
-    #negative_mask = get_negative_mask(batch_size)
+    # Mask out the positve mask from batch of Negative sample
+    negative_mask = get_negative_mask(batch_size)
 
     loss = 0
     for positives in [p_l2, z_l2]:
-        l_negative = tf.tensordot(tf.expand_dims(
+
+        # negative_loss = cosine_sim_2d(positives, negatives)
+        negative_loss = tf.tensordot(tf.expand_dims(
             positives, 1), tf.expand_dims(tf.transpose(negatives), 0), axes=2)
         l_labels = tf.zeros(batch_size, dtype=tf.int32)
-        l_neg = tf.boolean_mask(l_negative, negative_mask)
-        l_neg = tf.reshape(l_neg, (batch_size, -1))/temperature
-        logits = tf.concat([similarity, l_neg], axis=1)
-        loss += tf.keras.losses.SparseCategoricalCrossentropy(y_pred=logits, y_true=l_labels,
-                                                              from_logits=True, reduction=tf.keras.losses.Reduction.SUM)
+        l_neg = tf.boolean_mask(negative_loss, negative_mask)
+
+        l_neg = tf.reshape(l_neg, (batch_size, -1))
+        l_neg /= temperature
+
+        logits = tf.concat([pos_loss, l_neg], axis=1)  # [N, K+1]
+        tf.keras.losses.SparseCategoricalCrossentropy()
+        loss_ = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True, reduction=tf.keras.losses.Reduction.SUM)
+        loss += loss_(y_pred=logits, y_true=l_labels)
+
     loss = loss/(2*batch_size)
+
     return loss
+
 
 '''SimCLR Paper Nt-Xent Loss # SYMMETRIZED Loss'''
 # Nt-Xent Loss Symmetrized
+# Simple Implemenation the loss NOTED [Experimental design]
+
 
 def nt_xent_symmetrize_keras(p, z, temperature):
     # cosine similarity the dot product of p,z two feature vectors
@@ -166,16 +186,26 @@ def nt_xent_symmetrize_keras(p, z, temperature):
 
 '''BYOL SYMETRIZE LOSS'''
 # Symetric LOSS
+# Offical Implementation
+
+# Noted consider using the stop gradient here
+"""already configure in Custom training loop so 
+==-> you might not need the stop GRAD 
+"""
+
 
 def byol_symetrize_loss(p, z):
+    z = tf.stop_gradient(z)
     p = tf.math.l2_normalize(p, axis=1)  # (2*bs, 128)
     z = tf.math.l2_normalize(z, axis=1)  # (2*bs, 128)
 
     similarities = tf.reduce_sum(tf.multiply(p, z), axis=1)
     return 2 - 2 * tf.reduce_mean(similarities)
 
+
 '''Loss 2 SimSiam Model'''
 # Asymetric LOSS
+# offical Implementation
 
 
 def simsam_loss(p, z):
@@ -189,14 +219,78 @@ def simsam_loss(p, z):
     # equivalent to maximizing the similarity).
     return -tf.reduce_mean(tf.reduce_sum((p * z), axis=1))
 
+# Experimental testing Collapse Situation
+
 
 def simsam_loss_non_stop_Gr(p, z):
     # The authors of SimSiam emphasize the impact of
     # the `stop_gradient` operator in the paper as it
     # has an important role in the overall optimization.
-    #z = tf.stop_gradient(z)
+    # z = tf.stop_gradient(z)
     p = tf.math.l2_normalize(p, axis=1)
     z = tf.math.l2_normalize(z, axis=1)
     # Negative cosine similarity (minimizing this is
     # equivalent to maximizing the similarity).
     return -tf.reduce_mean(tf.reduce_sum((p * z), axis=1))
+
+
+######################################################################################
+'''NONE CONTRASTIVE + Online Clustering Base Approach'''
+####################################################################################
+'''
+Why not using OFFLINE cluster method to assign the Psudo -- Group of lable feature on entire dataset
+--> using this training in Supervised manner
+----->  THIS method has to go through entire data to generate all these subset label (expensive to implement)
+==> Need the Online Clustering for reduce the computation expensive of the method
+# Using Non-Contrastive + Clustering Assignment
+1. Building Prototype Net to Convert 256 Feature vector --> Smaller protype Vectors
+2. Bulding Sinkhorn For clustering Assignment (SwAV-- Online assigment with Learnable parameter)
+3. Cross entropy loss to optimize the encoder (2 version should be similar)
+'''
+# 1 Prototype Network
+
+
+# 2 Building the SInkhorn cluster assignment with 3 Itertion
+# Good number of iter for effcient runing in GPUs times
+
+def sinkhorn(sample_prototype_batch):
+
+    Q = tf.transpose(tf.exp(sample_prototype_batch/0.05))
+    Q /= tf.keras.backend.sum(Q)
+    K, B = Q.shape
+
+    u = tf.zeros_like(K, dtype=tf.float32)
+    r = tf.ones_like(K, dtype=tf.float32) / K
+    c = tf.ones_like(B, dtype=tf.float32) / B
+
+    for _ in range(3):
+        u = tf.keras.backend.sum(Q, axis=1)
+        Q *= tf.expand_dims((r / u), axis=1)
+        Q *= tf.expand_dims(c / tf.keras.backend.sum(Q, axis=0), 0)
+
+    final_quantity = Q / tf.keras.backend.sum(Q, axis=0, keepdims=True)
+    final_quantity = tf.transpose(final_quantity)
+
+    return final_quantity
+
+
+def SwAV_loss(tape, crops_for_assign, batch_size, NUM_CROPS, prototype, temperature):
+    loss = 0
+    for i, crop_id in enumerate(crops_for_assign):
+        with tape.stop_recording():
+            out = prototype[batch_size * crop_id: batch_size*(crop_id+1)]
+            # get assignment
+            q = sinkhorn(out)
+
+        # Cluster assigment predictiob
+        subloss = 0
+        for v in np.delete(np.arange(np.sum(NUM_CROPS)), crop_id):
+            p = tf.nn.softmax(
+                prototype[batch_size * v: batch_size * (v + 1)] / temperature)
+            subloss -= tf.math.reduce_mean(
+                tf.math.reduce_sum(q * tf.math.log(p), axis=1))
+        loss += subloss / tf.cast((tf.reduce_sum(NUM_CROPS) - 1), tf.float32)
+
+    loss /= len(crops_for_assign)
+
+    return loss
