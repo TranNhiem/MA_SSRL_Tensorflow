c9139c7ee57e1d7cc808f7a08ca481f459ae0cde
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-10-17 13:13:41 +0800

    commit

--- self_supervised_learning_frameworks/contrastive_positve_negative_pair/contrastive_simclr.py
+++ self_supervised_learning_frameworks/contrastive_positve_negative_pair/contrastive_simclr.py
@@ -1,3 +1,7 @@
 '''
 Implementation Contrastive Leanring Framework (SimCLR for Standard Implementation)
+Demonstrated of the implementation can found in this Repository 
+
+https://github.com/TranNhiem/semi_self_supervised_project/blob/master/Conv_ViT_SSL_Contrastive_Test.py
+
 '''
--- self_supervised_learning_frameworks/non_contrastive/Non_contrastive_framework.py
+++ self_supervised_learning_frameworks/non_contrastive/Non_contrastive_framework.py
@@ -1,5 +1,9 @@
 '''
-the implementation of Non_Contrastive Framework 
+the implementation of Non_Contrastive Framework
+ 
 BYOL for the stand implementaiton 
 
+The Implementation Can be found in this Repository 
+
+https://github.com/TranNhiem/semi_self_supervised_project/blob/master/Conv_ViT_SSL_Non_Contrastive_Test.py 
 '''
