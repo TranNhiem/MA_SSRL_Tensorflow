94b3c5fa322af526f079b34d89747f5b95d057b3
HunagChiEn <a3285556a@gmail.com>
2021-10-21 12:40:55 +0800

    merge Auto_Augment.py & RandAugment.py into single class Data_Augumentor

--- Augmentation_Strategies/Auto_Augment.py
+++ Augmentation_Strategies/Auto_Augment.py
@@ -1,49 +1,107 @@
 __src_str__ = '''
-  Auto Augmentation Policy Focus on Object Detection Task 
-  #Reference Implementation on Object Detection with 
-  1. AutoAugment 3 Policies (V0- V3)
-  Barret, et al. Learning Data Augmentation Strategies for Object Detection.
-      Arxiv: https://arxiv.org/abs/1906.11172
-  2. RandomAugment --> Also apply for object Detection Models
+Data Augumentation Searching Package :
+  1. Auto Augmentation Policy Focus on Object Detection Task 
+    #Reference Implementation on Object Detection with 
+    (1). AutoAugment 3 Policies (V0- V3)
+    Barret, et al. Learning Data Augmentation Strategies for Object Detection.
+        Arxiv: https://arxiv.org/abs/1906.11172
+    (2). RandomAugment --> Also apply for object Detection Models
 
-  ## Reference GitHub for implementation
-  [1] https://github.com/google/automl/blob/master/efficientdet/aug/autoaugment.py
-  [2] https://github.com/tensorflow/models/blob/master/official/vision/image_classification/augment.py
+    ## Reference GitHub for implementation
+    [1] https://github.com/google/automl/blob/master/efficientdet/aug/autoaugment.py
+    [2] https://github.com/tensorflow/models/blob/master/official/vision/image_classification/augment.py
+
+  2.RandAug implementation for Self-Supervised Learning
+    paper: https://arxiv.org/abs/1909.13719
+    RandAug with 2 Version : 
+        Version 1 from Original Paper (14 Transformations )
+        Version 2 Modify with Adding Multiple Transformation (22 transformation from Imgaug API)
 '''
 
 # Current implementation will Deploy for Images WITHOUT BOX
 import tensorflow as tf
 from tensorflow.image import random_flip_left_right, random_crop
-from official.vision.image_classification.augment import AutoAugment
+from official.vision.image_classification.augment import AutoAugment, RandAugment
+import imgaug.augmenters as iaa
+
+# For 'Type Hint' function, we replace the comment of code into type package.. 
+#from typing import Any, Dict, List, Optional, Text, Tuple
+from functools import partial
+import numpy as np
+
 
 class Data_Augumentor(object):
     # static instance, declare once use everywhere!
-    aug_inst = AutoAugment(augmentation_name='v0')
-    def __init__(self):  
+    DAS_dict = {"auto_aug":AutoAugment, "tf_rand_aug":RandAugment, "iaa_rand_aug":iaa.RandAugment}
+    # common method name to apply the data augmentation!!
+    DA_METHOD = "distort"
+    def __init__(self, DAS_type="auto_aug", *aug_args, **aug_kwarg):
+        try:
+            self.DAS_type = DAS_type
+            self.aug_inst = DAS_dict[DAS_type](*aug_args, **aug_kwarg)
+        except KeyError as k_err:
+            raise KeyError("Given vlaue {} of DAS_type, \
+                            but the value should be one of that ({})".format(
+                            aug_type, DAS_dict.keys() )
+                          )
+        except Exception as exc:
+            print(exc)
+
         self.pre_proc_dict = {"default":lambda image, *_ : tf.cast(image, dtype=tf.float32),
                     "rnd_crp_flp":self.rand_crop_flip,
                     "glb_loc_crp_flp":self.rand_distribe_crop_global_local_views_flip}
         self.post_proc = lambda image : tf.cast(image, dtype=tf.float32) / 255.
-
+        self.regist_common_distort()
 
     @staticmethod
-    def prnt_policies():
-        print("AutoAugment Policy V0-- implementation : \n")
-        print("V0--> policy = \n")
-        print(Data_Augumentor.aug_inst.policy_v0())
-        print("\n\n=================\n\n")
-        print("Policy_simple = \n")
-        print(Data_Augumentor.aug_inst.policy_simple())
+    def prnt_policies(DAS_type):
+        if DAS_type == "auto_aug":
+            print("AutoAugment Policy V0-- implementation : \n")
+            print("V0--> policy = \n")
+            print(DAS_dict[DAS_type].policy_v0())
+            print("\n\n=================\n\n")
+            print("Policy_simple = \n")
+            print(DAS_dict[DAS_type].policy_simple())
+
+        elif DAS_type == "tf_rand_aug":
+            print("Tensorflow RandAugment avaliable ops : \n")
+            print(DAS_dict[DAS_type].available_ops)
+
+        # GG.. ImageAug did not offer any public method to get the avaliable_ops
+        elif DAS_type == "iaa_rand_aug": 
+            print("ImageAug RandAugment avaliable ops : \n")
+            aug_str = '''[ Fliplr, KeepSizeByResize, Crop, Sequential, SomeOf, Identity, \
+                            Autocontrast, Equalize, Invert, Affine, Posterize, Solarize, EnhanceColor, \
+                            EnhanceContrast,EnhanceBrightness, EnhanceSharpness, Cutout, FilterBlur, FilterSmooth]'''
+            print(aug_str)
+        else:
+            raise ValueError("Given vlaue {} of DAS_type, \
+                            but the value should be one of that ({})".format(
+                            aug_type, DAS_dict.keys() )
+                          )
+
+    #def get_preproc_spec(self):
+     #   return {}
+
+    # allow the augument instance have common method name to apply the data transformation
+    def regist_common_distort(self):
+        # the distort method already implemented in augument instance
+        if Data_Augumentor.DA_METHOD in dir(self.aug_inst):
+            return
+        # plz regist the correct method, which apply the data transformation.. 
+        if self.DAS_type == "iaa_rand_aug":
+            self.aug_inst.distort = self.aug_inst.__call__
+
 
     def rand_crop_flip(self, image, crop_size):
         '''
-        Args: 
-            image: A tensor [ with, height, channels]
-            crop_size: Apply Random Crop_Flip Image before Apply AutoAugment
-            AutoAugment: a function to apply Policy transformation [v0, policy_simple]
+            Args: 
+                image: A tensor [ with, height, channels]
+                crop_size: Apply Random Crop_Flip Image before Apply AutoAugment
+                AutoAugment: a function to apply Policy transformation [v0, policy_simple]
 
             Return: 
-            Image: A tensor of Applied transformation [with, height, channels]
+                Image: A tensor of Applied transformation [with, height, channels]
         '''
         crp_shap = (crop_size, crop_size, 3)
         flp_img = tf.image.random_flip_left_right(image)
@@ -54,13 +112,13 @@ class Data_Augumentor(object):
     def rand_distribe_crop_global_local_views_flip(image, crop_size, min_scale, max_scale, high_resol=True):
         '''
             Args:
-            image: A tensor [ with, height, channels]
-            crop_size: Rand --> Flipping --> random_distribute_uniform (min_scale, max_scale) 
-            high_resol --> True: For Global crop_view, False: For Local crop views
-            AutoAugment: a function to apply AutoAugment transformation 
+                image: A tensor [ with, height, channels]
+                crop_size: Rand --> Flipping --> random_distribute_uniform (min_scale, max_scale) 
+                high_resol --> True: For Global crop_view, False: For Local crop views
+                AutoAugment: a function to apply AutoAugment transformation 
 
             Return: 
-            Image: A tensor of Applied transformation [with, height, channels]
+                Image: A tensor of Applied transformation [with, height, channels]
         '''
         flp_img = tf.image.random_flip_left_right(image)
         crp_ratio = crop_size * 1.4 if high_resol else crop_size * 0.8  
@@ -79,19 +137,34 @@ class Data_Augumentor(object):
 
     def data_augment(self, image, aug_type="default", crop_size=None, 
                 min_scale=None, max_scale=None, high_resol=True):
+        '''
+            The common interface for public user, which allow user only call this method 
+            to augument the given image with the corresponding behavior setting by args. 
+            Args:
+                image: A tensor [ with, height, channels]
+                other args for various augumentation
+            Return: 
+                Image: A tensor of Applied transformation [with, height, channels]
+        '''
+        def distort(pre_img):
+            if self.DAS_type == "iaa_rand_aug":
+                return self.aug_inst(pre_img)
+            return self.aug_inst.distort(pre_img)
+        
         try:
             pre_img = self.pre_proc_dict[aug_type](image, crop_size, min_scale, 
                                     max_scale, high_resol)
         except KeyError as k_err:
             raise KeyError("Given vlaue {} of aug_type, \
             but the value should be one of that ({})".format(
-                aug_type, ["default", "rnd_crp_flp", "glb_loc_crp_flp"]
-            ))
+                aug_type, self.pre_proc_dict.keys() )
+            )
         except Exception as exc:
-            print(exc)
+            print(exc) ; return
 
-        aug_img = Data_Augumentor.aug_inst.distort(pre_img)
+        aug_img = self.aug_inst.distort(pre_img)
         return self.post_proc(aug_img)
+
 
 if __name__ == '__main__':
     # simple test
@@ -101,4 +174,3 @@ if __name__ == '__main__':
     dummy_img = np.random.random((3, 3, 4))
     aug_img = da_inst.data_augment(dummy_img)
     print(aug_img)
-
