46c839c525636e3e17df2f8c8889bcf9347369d6
HunagChiEn <a3285556a@gmail.com>
2021-12-04 17:02:31 +0800

    un-warp dataset style of multiivew & unmix implementation

---
+++ Augmentation_Strategies/View_Mixer/View_mixer.py
@@ -0,0 +1,21 @@
+def sample_beta_distribution(size, concentration_0=0.1, concentration_1=0.1):
+    gamma_1_sample = tf.random.gamma(shape=[size], alpha=concentration_1)
+    gamma_2_sample = tf.random.gamma(shape=[size], alpha=concentration_0)
+    return gamma_1_sample / (gamma_1_sample + gamma_2_sample)
+
+def mix_up_batch(ds_one, ds_two, alpha=0.2):
+    # Unpack two datasets
+    images_one, labels_one = ds_one
+    images_two, labels_two = ds_two
+    batch_size = tf.shape(images_one)[0]
+
+    # Sample lambda and reshape it to do the mixup
+    lamda = sample_beta_distribution(batch_size, alpha, alpha)
+    x_l = tf.reshape(l, (batch_size, 1, 1, 1))
+    y_l = tf.reshape(l, (batch_size, 1))
+
+    # Perform mixup on both images and labels by combining a pair of images/labels
+    # (one from each dataset) into one image/label
+    images = images_one * x_l + images_two * (1 - x_l)
+    labels = labels_one * y_l + labels_two * (1 - y_l)
+    return (images, lamda)
--- Augmentation_Strategies/main.py
+++ Augmentation_Strategies/main.py
@@ -3,22 +3,19 @@ import tensorflow_datasets as tfds
 import tensorflow as tf
 from vis_tool import imshow_imgs
 from matplotlib import pyplot as plt
-
-# import global / local view function
 from functools import partial 
-from Simclr_Byol_augmentation import (flip_random_crop, random_crop_flip_resize, 
-                    inception_style_croping, rand_distribe_crop_global_local_views_flip)
 
 # self-def pkg
 from Auto_Data_Augment.Data_Augmentor import Data_Augmentor
 from Multi_Viewer.Multi_viewer import Multi_viewer
-
+from View_Mixer.View_mixer import mix_up_batch
 
 def vis_data_aug(ds, da_type="auto_aug", batch_size=4):
     da = Data_Augmentor(da_type)
     da.pre_proc_dict["default"] = lambda x : tf.cast(x, dtype=tf.float32) * 255.0 
     mv = Multi_viewer(da_inst=da)
 
+    ## Multi-view + Un-mix strategy un-loop style:
     for example in ds.batch(batch_size, drop_remainder=True):
         bh_img, lab = example["image"], example["label"]
         imshow_imgs( bh_img.numpy() )
@@ -27,6 +24,11 @@ def vis_data_aug(ds, da_type="auto_aug", batch_size=4):
         # output global view
         imshow_imgs(mv_bh_img[0], "global_view examples")
         imshow_imgs(mv_bh_img[3], "local_view examples")
+        # global view mixing
+        glb_mix = mix_up_batch(mv_bh_img[0], mv_bh_img[1])
+        # local view mixing with pair
+        loc_mix_23 = mix_up_batch(mv_bh_img[2], mv_bh_img[3])
+        loc_mix_24 = mix_up_batch(mv_bh_img[2], mv_bh_img[4])
         break
 
 
@@ -43,5 +45,3 @@ def get_toy_dataset(dataset_name, split='train', shuffle_files=True, with_info=T
 if __name__ == "__main__":
     cifar_ds, info = get_toy_dataset("cifar10")
     vis_data_aug(cifar_ds)
-    
-    
