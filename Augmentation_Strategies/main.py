b563b8daee31871be75cda49dfc14dc4fe0d82e7
HunagChiEn <a3285556a@gmail.com>
2021-12-08 22:42:58 +0800

    complete un-mix strategy

--- Augmentation_Strategies/main.py
+++ Augmentation_Strategies/main.py
@@ -1,14 +1,28 @@
+# PEP8 : alphabet order 
+import copy
+from functools import partial 
+from matplotlib import pyplot as plt
 import numpy as np
+
 import tensorflow_datasets as tfds
 import tensorflow as tf
-from vis_tool import imshow_imgs
-from matplotlib import pyplot as plt
-from functools import partial 
+AUTOTUNE = tf.data.experimental.AUTOTUNE  # global define var for ds.map parallel call
 
 # self-def pkg
 from Auto_Data_Augment.Data_Augmentor import Data_Augmentor
 from Multi_Viewer.Multi_viewer import Multi_viewer
 from View_Mixer.View_mixer import mix_up_batch
+from vis_tool import imshow_imgs
+
+def get_toy_dataset(dataset_name, split='train', shuffle_files=True, with_info=True):
+    ds_lst = tfds.list_builders()
+    assert dataset_name in ds_lst  
+    # self-supervised without label
+    ds, meta_info = tfds.load(dataset_name, split=split, shuffle_files=shuffle_files, 
+                                with_info=with_info, as_supervised=False)
+    ds_pkg = (ds, meta_info) if with_info else ds
+    return ds_pkg
+
 
 def multiview_wrap(ds, da_type="auto_aug"):
     # preproc for cifar10
@@ -23,39 +37,36 @@ def multiview_wrap(ds, da_type="auto_aug"):
     out_typ_lst = [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32]
     py_flow_wrap = lambda x : tf.py_function(mv.multi_view, [x], Tout=out_typ_lst)
     return ds.map(py_flow_wrap, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+   
+def view_mixing_wrap(ds):
+
+    def mix_view(*args):
+        v01, l01 = mix_up_batch(args[0], args[1]) 
+        v23, l23 = mix_up_batch(args[2], args[3])
+        v24, l24 = mix_up_batch(args[2], args[4])
+        return (v01, v23, v24), (l01, l23, l24)
+
+    return ds.map(mix_view, num_parallel_calls=tf.data.experimental.AUTOTUNE)   
     
 
-def vis_data_aug(ds, batch_size=4):
+def vis_data_aug(ori_ds, batch_size=4):
     # perform batch before calling the map method benefit the training performance
+    ds = ori_ds   # retain intermediate results
     ds = ds.batch(batch_size, drop_remainder=True)
-    ds = multiview_wrap(ds)
-
-    for mv_bh_img in ds:
-        print(f"view size : { len(mv_bh_img) }\n")
-        # output global view
-        imshow_imgs(mv_bh_img[0].numpy(), "global_view examples")
-        imshow_imgs(mv_bh_img[3].numpy(), "local_view examples")
-
-        # perform un-mix strategy
-        '''
-        #   global view mixing
-        glb_mix = mix_up_batch(mv_bh_img[0], mv_bh_img[1])
-        #   local view mixing with pair
-        loc_mix_23 = mix_up_batch(mv_bh_img[2], mv_bh_img[3])
-        loc_mix_24 = mix_up_batch(mv_bh_img[2], mv_bh_img[4])
-        imshow_imgs(mv_bh_img[0].numpy(), "global_view examples")
-        imshow_imgs(mv_bh_img[3].numpy(), "local_view examples")
-        '''
-
-def get_toy_dataset(dataset_name, split='train', shuffle_files=True, with_info=True):
-    ds_lst = tfds.list_builders()
-    assert dataset_name in ds_lst  
-    # self-supervised without label
-    ds, meta_info = tfds.load(dataset_name, split=split, shuffle_files=shuffle_files, 
-                                with_info=with_info, as_supervised=False)
-    ds_pkg = (ds, meta_info) if with_info else ds
-    return ds_pkg
-
+    
+    ## Kernel of the proposed method : multi-view & view mixing strategies 
+    mv_ds = multiview_wrap(ds)
+    mix_ds = view_mixing_wrap(mv_ds)
+    
+    # for loop-style solution (pass the test)
+    for  bh_mix_img, lam in mix_ds.take(1):
+        print(f"multiple view size : { len(bh_mix_img) }\n")
+        # visualized example..
+        imshow_imgs( bh_mix_img[0].numpy() , "global view mixed examples")
+        imshow_imgs( bh_mix_img[1].numpy() , "local mixed 2, 3 view examples")
+        imshow_imgs( bh_mix_img[2].numpy() , "local mixed 2, 4 view examples")
+    
 
 if __name__ == "__main__":
     cifar_ds, info = get_toy_dataset("cifar10")
