59960f587a085377f43519cf832bc03a41df5c8b
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-28 16:59:33 +0800

    model_save_dir

--- config/config_non_contrast.py
+++ config/config_non_contrast.py
@@ -331,7 +331,7 @@ def Configure_Saving_and_Restore_Model():
     # Saving Model
     flags = Mock_Flag()
     flags.DEFINE_string(
-        'model_dir', "/data/resnet_byol/",
+        'model_dir', "./model_ckpt/resnet_byol/resnet_50/",
         'Model directory for training.')
 
     flags.DEFINE_integer(
