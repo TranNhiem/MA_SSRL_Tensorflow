77d3377b434f2c013f558b01f609ca931ada9125
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-01-17 13:48:13 +0800

    configre

--- config/non_contrast_config_v1.py
+++ config/non_contrast_config_v1.py
@@ -32,7 +32,7 @@ def read_cfg_base(mod="non_contrastive"):
     # two options [fixed_value, schedule] schedule recommend from BYOL
     FLAGS.moving_average = "schedule"
     # ['fp16', 'fp32'],  # fp32 is original precision
-    FLAGS.mixprecision = 'fp32'
+    FLAGS.mixprecision = 'FP32'
     # , [ 'original', 'model_only', ],
     FLAGS.XLA_compiler = "original"
     FLAGS.base_lr = 0.3
